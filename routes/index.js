03bfac62907b735560491157b538640787e7c066
Fabrizzio Rivera <frivera@nevicad.com>
2020-09-06 14:25:52 -0600

    full pages

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,24 @@ var express = require('express')
 var router = express.Router()
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
+router.get('/', (req, res, next) => {
+  res.render('track', {
+    owners: [
+      { id: 1, name: 'FERCO' },
+      { id: 2, name: 'Arium' },
+      { id: 3, name: 'Alza' },
+      { id: 4, name: 'Cendalza' },
+      { id: 5, name: 'You (Break/Lunch)' }
+    ]
+  })
+})
+
+router.get('/team', (req, res, next) => {
+  res.render('team', {})
+})
+
+router.get('/detail', (req, res, next) => {
+  res.render('detail', {})
 })
 
 module.exports = router
