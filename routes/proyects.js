f344bbdf6c348e34899ca533e3a84b44f4c7fd3a
Fabrizzio Rivera <frivera@nevicad.com>
2020-09-06 17:19:59 -0600

    proyects post

--- routes/proyects.js
+++ routes/proyects.js
@@ -42,4 +42,45 @@ router.get('/', (req, res, next) => {
   })
 })
 
+router.post('/', (req, res, next) => {
+  const colors = ['bg-primary', 'bg-secondary', 'bg-danger', 'bg-warning', 'bg-info']
+  res.status(201).render('proyects', {
+    title: 'Timetracker - Proyects',
+    owners: [
+      { id: 1, name: 'FERCO' },
+      { id: 2, name: 'Arium' },
+      { id: 3, name: 'Alza' },
+      { id: 4, name: 'Cendalza' }
+    ],
+    proyects: [
+      {
+        id: 1,
+        name: 'Soporte Ferco',
+        description: 'Proyecto sobre el soporte a FERCO',
+        hours: 100,
+        times: [
+          {
+            user: 'Fernando Rodrigez',
+            color: colors[0],
+            hours: 15,
+            percent: (15 / 91) * 100
+          },
+          {
+            user: 'Maria Rivera',
+            color: colors[4],
+            hours: 44,
+            percent: (44 / 91) * 100
+          },
+          {
+            user: 'Juana Gonzalez',
+            color: colors[2],
+            hours: 32,
+            percent: (32 / 91) * 100
+          }
+        ]
+      }
+    ]
+  })
+})
+
 module.exports = router
