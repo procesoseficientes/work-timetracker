a5130f13a8f26c4e6fa9fc769f0c361694a3be29
Fabrizzio Rivera <frivera@nevicad.com>
2020-12-21 11:16:19 -0600

    removed uneded files

--- dist/services/DbService.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class DbService {
-    constructor(client) {
-        this.client = client;
-    }
-}
-exports.default = DbService;
