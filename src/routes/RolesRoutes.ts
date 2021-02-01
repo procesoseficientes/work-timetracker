26433736ff04372029eb9aef4cd415b91e550e38
fabrv <frivera@nevicad.com>
2021-02-01 10:13:01 -0600

    toggle for all views

--- src/routes/RolesRoutes.ts
+++ src/routes/RolesRoutes.ts
@@ -57,6 +57,21 @@ export function RolesRoutes (pgClient: Client): Router {
     .catch(err => next(createHttpError(500, err.message)))
   })
 
+  router.put('/api/:id/toggle', hasAccess('update', roleService), (req, res) => {
+    roleService.toggleState(parseInt(req.params.id)).then(data => {
+      res.send({
+        status: 200,
+        data: data
+      })
+    }).catch(err => {
+      console.error(err)
+      res.status(500).send({
+        status: 500,
+        message: err.message
+      })
+    })
+  })
+
   router.get('/:id', hasAccess('read', roleService), async (req, res) => {
     res.render('roles/role', {
       title: `Timetracker - Roles`,
