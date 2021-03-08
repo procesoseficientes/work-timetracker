7b972bb50655bc0987f8fff3a5eaf6b8e1be18ac
fabrv <frivera@nevicad.com>
2021-03-08 09:21:46 -0600

    fixes bug again

--- src/app.ts
+++ src/app.ts
@@ -52,7 +52,7 @@ app.use(session({
 
 const pgClient = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: true
+  ssl: { rejectUnauthorized: false }
 })
 pgClient.connect().catch((err) => {
   console.error(err)
