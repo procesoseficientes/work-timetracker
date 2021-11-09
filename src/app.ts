1a7b68fabc69a83b92fe40bc632d41ba27cf580d
fabrv <frivera@nevicad.com>
2021-11-08 22:04:22 -0600

    removed body parser dep

--- src/app.ts
+++ src/app.ts
@@ -49,8 +49,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, '../public')))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(session({
   secret: 'secret',
   resave: true,
