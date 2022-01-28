60c6e00743a46b40cb230cde7e7aeedbc76b6cc0
fabrv <fabrizzio.rivera@telus.com>
2022-01-28 15:11:33 -0600

    update deps and xls and PDF export

--- src/routes/DetailRoutes.ts
+++ src/routes/DetailRoutes.ts
@@ -2,11 +2,14 @@ import { Router } from 'express'
 import { Client } from 'pg'
 import { sidebarComponent } from '../components/sidebar/sidebar'
 import TimeService from '../controllers/TimeService'
-import { Parser } from 'json2csv'
+// import { Parser } from 'json2csv'
 import { hasAccess } from '../utils/auth'
 import { RoleService } from '../controllers/RoleService'
 import createHttpError from 'http-errors'
 import { Converter } from 'showdown'
+import { DetailReport } from '../components/detailReport/detailReport'
+
+import { mdToPdf } from 'md-to-pdf'
 
 export function DetailRoutes(pgClient: Client): Router {
   const router: Router = Router()
@@ -73,36 +76,70 @@ export function DetailRoutes(pgClient: Client): Router {
     )
   })
 
-  router.get('/excel', hasAccess('read', roleService), async (req, res, next) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  router.get('/excel', hasAccess('read', roleService), (req, res: any, next) => {
     if (!req.query.page || req.query.page === '') req.query.page = '0'
     timeService.getTimes(
-      <string>req.query.name,
-      <string>req.query.owner,
-      <string>req.query.project,
-      <string>req.query.from === '' ? undefined : <string>req.query.from,
-      <string>req.query.to === '' ? undefined : <string>req.query.to,
+      req.query.name?.toString(),
+      req.query.owner?.toString(),
+      req.query.project?.toString(),
+      req.query.from?.toString() === '' ? undefined : req.query.from?.toString(),
+      req.query.to?.toString() === '' ? undefined : req.query.to?.toString(),
       0,
       10000
     ).then(data => {
       // @fabrv
-      // JSONtoCSV crahses if the input date is empty
+      // JSONtoXls crahses if the input date is empty
       // This will line will create an empty row so JSONtoCSV can work
       data = data.length === 0 ? [{ name: '', owner: '', project: '', task: '', start: '', end: '', hours: ''}] : data
 
-      const parser = new Parser()
-      const csv = parser.parse(data)
-
-      res.writeHead(200, {
-        'Content-Disposition': `attachment; filename="Times.csv"`,
-        'Content-Type': 'text/csv',
-      })
-
-      res.end(csv)
+      res.xls(`${req.query.name}-${req.query.from}-${req.query.to}.xlsx`, data)
 
     }).catch(err => {
       console.error(err)
       next(createHttpError(500, err.message))
     })
+  })
+
+  router.get('/pdf', hasAccess('read', roleService), async (req, res) => {
+    const times = (await timeService.getTimes(
+      req.query.name?.toString(),
+      req.query.owner?.toString(),
+      req.query.project?.toString(),
+      req.query.from?.toString() === '' ? undefined : req.query.from?.toString(),
+      req.query.to?.toString() === '' ? undefined : req.query.to?.toString(),
+      0,
+      10000
+    )).map(time => {
+      time.start = (new Date(time.start)).toISOString().substring(0, 10),
+      time.end = (new Date(time.end)).toISOString().substring(0, 10)
+      time.task = time.task.slice(0, 15) + (time.task.length > 15 ? '...' : '')
+      return time
+    })
+
+    const data = times.length === 0 ? [{ name: '', owner: '', project: '', task: '', start: '', end: '', hours: ''}] : times
+
+    const reportComponent = new DetailReport({
+      name: req.query.name?.toString() || '',
+      owner: req.query.owner?.toString() || '',
+      project: req.query.project?.toString() || '',
+      end: req.query.to?.toString() || '',
+      start: req.query.from?.toString() || '',
+      hours: '',
+      task: '',      
+      data: data,
+      totalHours: data.reduce((acc, curr) => acc + parseFloat(curr.hours), 0)
+    })
+
+    const pdf = await mdToPdf({content: reportComponent.render()}).catch(console.error)
+
+    res.writeHead(200, {
+      'Content-Disposition': `attachment; filename="Times.pdf"`,
+      'Content-Type': 'application/pdf',
+    })
+
+    res.end(pdf?.content)
+    //res.send(new Converter({tables: true, simpleLineBreaks: true}).makeHtml(reportComponent.render()))
   })
 
   return router
